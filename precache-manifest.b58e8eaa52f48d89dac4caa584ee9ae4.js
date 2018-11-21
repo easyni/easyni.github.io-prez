8b369b84c383f11243a2cf5220a7daa339bd53ed
Nicolas Ribes <nribes@wynd.eu>
2018-11-21 18:19:22 +0100

    first commit

---
+++ precache-manifest.b58e8eaa52f48d89dac4caa584ee9ae4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f83fdb3e441ed07cb2b",
+    "url": "/static/css/main.ff6be9fb.chunk.css"
+  },
+  {
+    "revision": "2f83fdb3e441ed07cb2b",
+    "url": "/static/js/main.2f83fdb3.chunk.js"
+  },
+  {
+    "revision": "ac5250569b4a1b60f8ac",
+    "url": "/static/css/1.2a3fc7c3.chunk.css"
+  },
+  {
+    "revision": "ac5250569b4a1b60f8ac",
+    "url": "/static/js/1.ac525056.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "443c7d7ca7a2d145a8c796e8e8de951a",
+    "url": "/index.html"
+  }
+];
